8b792b17873410967ca167350c739e886c7345f7
sijirama <gbemilesanmi@gmail.com>
2024-11-09 00:42:04 +0100

    landing

--- src/app/components/custom/ShinyButton.tsx
+++ src/app/components/custom/ShinyButton.tsx
@@ -10,7 +10,7 @@ export default function ShinyButton({ className, children, href, ...props }: Shi
     return (
         <Link href={href ?? "#"}
             className={cn(
-                "group relative flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-brand-700 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-brand-700 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2 py-0.5",
+                "group relative flex transform items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-brand-700 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-brand-700 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2 ",
                 className
             )}
             {...props}
