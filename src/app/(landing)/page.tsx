03cde41958e7638f8f576ae7b59efef31378c66e
sijirama <gbemilesanmi@gmail.com>
2024-11-09 02:59:28 +0100

    init

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,6 +4,7 @@ import { MaxWidthWrapper } from '@/components/custom/max-width-wrapper'
 import { Heading } from '@/components/custom/Heading'
 import { Check } from 'lucide-react'
 import ShinyButton from '@/components/custom/ShinyButton'
+import MockDiscordUI from '@/components/custom/MockDiscordUI'
 
 export default function Page() {
     return (
@@ -39,8 +40,17 @@ export default function Page() {
                 </MaxWidthWrapper>
             </section>
 
-            <section></section>
-            <section></section>
+            <section className='relative bg-brand-25 pb-4 '>
+                <div className='absolute inset-x-0 bottom-24 top-24 bg-brand-700 ' />
+                <div className='relative mx-auto'>
+                    <MaxWidthWrapper className='relative'>
+                        <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 '>
+                            <MockDiscordUI />
+                        </div>
+                    </MaxWidthWrapper>
+                </div>
+            </section>
+
             <section></section>
             <section></section>
         </>
