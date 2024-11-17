0de8926dfc7b92f27166b0cb6c9856bbac49a2c6
sijirama <gbemilesanmi@gmail.com>
2024-11-17 23:22:09 +0100

    fix: deploy

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,4 +1,3 @@
 import { httpHandler } from "@/server"
 export const runtime = "edge"
-export const dynamic = "force-dynamic"  // Add this to ensure dynamic routing
-export { httpHandler as GET, httpHandler as POST, httpHandler as PUT, httpHandler as DELETE }
+export { httpHandler as GET, httpHandler as POST }
