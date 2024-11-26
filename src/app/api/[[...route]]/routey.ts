42d2894f1f4d35f0e1547eafa63ad5b25555d61c
sijirama <gbemilesanmi@gmail.com>
2024-11-26 22:02:30 +0100

    deploy

--- src/app/api/[[...route]]/routey.ts
+++ src/app/api/[[...route]]/routey.ts
@@ -1,3 +1,3 @@
-// import { httpHandler } from "@/server"
+import { httpHandler } from "@/server"
 // export const runtime = "edge"
-// export { httpHandler as GET, httpHandler as POST }
+export { httpHandler as GET, httpHandler as POST }
